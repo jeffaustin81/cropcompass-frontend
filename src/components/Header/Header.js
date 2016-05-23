017d4e9cea93fe54e8cb4629bef74dd6d4f2365c
Jeff Austin <jeffaustin81@gmail.com>
2016-05-22 18:27:14 -0700

    Add header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,14 +4,26 @@ import classes from './Header.scss'
 
 export const Header = () => (
   <div>
-    <h1>React Redux Starter Kit</h1>
-    <IndexLink to='/' activeClassName={classes.activeRoute}>
-      Home
-    </IndexLink>
-    {' · '}
-    <Link to='/counter' activeClassName={classes.activeRoute}>
-      Counter
-    </Link>
+    <header>
+      <div className="logo">
+        <img src="../../CC-logo.png" alt="CropCompass logo" width="200" height="100" />
+      </div>
+      <nav>
+        <ul>
+
+        <li><a href="#">
+          <img src="../../icons/crop-header-icons-off-white/location-iconx2.png" alt="Location Icon" width="23" height="30"/>
+            <p>Choose County</p>
+        </a></li>
+
+        <li><a href="#">
+          <img src="../../icons/crop-header-icons-off-white/crop-apple2x.png" alt="Crop Icon" width="35" height="35"/>
+            <p>Choose Crop</p>
+        </a></li>
+
+        </ul>
+      </nav>
+    </header>
   </div>
 )
 
