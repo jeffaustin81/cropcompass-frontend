df45978b1be5fb2bf3e66becc00df9bd10effa2d
Jeff Austin <jeffaustin81@gmail.com>
2016-05-22 21:08:57 -0700

    Add styles

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -2,13 +2,13 @@ import React from 'react'
 import Header from '../../components/Header'
 import classes from './CoreLayout.scss'
 import '../../styles/core.scss'
-import SideMenu from '../../components/SideMenu/SideMenu'
+// import SideMenu from '../../components/SideMenu/SideMenu'
 
 export const CoreLayout = ({ children }) => (
   <div>
     <Header />
 
-</div>
+  </div>
 )
 
 CoreLayout.propTypes = {
