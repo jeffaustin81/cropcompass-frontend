e7f6f67016e921ea84431ff1e319fd691088bce5
dpcairns <dpcairns@gmail.com>
2016-06-03 12:44:30 -0700

    added first shot at horizontal bar chart. in components/HorizontalBarChar. Expects props: title, data, where data is an array of {cropName: carrots, cropNumber: 40555} objects

---
+++ src/components/HorizontalBarChart/HorizontalBarChart.js
@@ -0,0 +1,47 @@
+import { default as React, PropTypes } from 'react';
+import Bar from './Bar'
+export default class HorizontalBarChart extends React.Component{
+
+  render(){
+
+    let topCrops = [
+      {
+        cropName: "corn",
+        cropNumber: 45000
+      },
+      {
+        cropName: "carrots",
+        cropNumber: 33000
+      },
+      {
+        cropName: "rutabegas",
+        cropNumber: 25000
+      },
+      {
+        cropName: "potatoes",
+        cropNumber: 20000
+      },
+      {
+        cropName: "asparagus",
+        cropNumber: 5000
+      },
+    ]
+    console.log(topCrops)
+    let data = {}
+    let title = "Senators' favorite crops by grams consumed yearly"
+    let sumTotalofTopFive = topCrops.map(function(crop){
+      return crop.cropNumber
+    }).reduce(function(last, current){
+      return last + current
+    })
+    let barNodes = topCrops.map(function(crop, index){
+      return (<Bar cropName={crop.cropName} position={index} cropNumber={crop.cropNumber} sumTotalofTopFive={sumTotalofTopFive} />)
+    })
+    return(
+      <div>
+      <h1>{title}</h1>
+      {barNodes}
+      </div>
+    )
+  }
+}
