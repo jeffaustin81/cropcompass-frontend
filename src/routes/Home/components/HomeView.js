3bd8dd259ad22ba2f57d8e5588073f7b283cfca1
Jeff Austin <jeffaustin81@gmail.com>
2016-06-03 21:30:52 -0700

    Move components to HomeView

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import Map from 'components/Map'
+import FarmedLand from 'components/FarmedLand'
+import FarmInfo from 'components/FarmInfo'
+import TopCrops from 'components/TopCrops'
+import Subsidies from 'components/Subsidies'
+import CropProduction from 'components/CropProduction'
+import ImportExport from 'components/ImportExport'
 import HorizontalBarChart from 'components/HorizontalBarChart/HorizontalBarChart'
 
 export const HomeView = () => {
@@ -14,9 +20,14 @@ export const HomeView = () => {
 
   return (
     <div>
-      <h4>Welcome!</h4>
       <Map countyColors={someArray} width={'500px'} height={'500px'}
         selectedCounty={'41'} onCountySelect={handleCountySelect} />
+        <FarmedLand />
+        <FarmInfo />
+        <TopCrops />
+        <Subsidies />
+        <CropProduction />
+        <ImportExport/>
       <HorizontalBarChart />
     </div>)
 }
