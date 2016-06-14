79d9fd71c574c5847bde8823479ca7943cf1794d
dpcairns <dpcairns@gmail.com>
2016-06-14 15:17:52 -0700

    (non-vector) image added to donut

--- src/components/VisualizationsD3/DonutD3/TryDonut.js
+++ src/components/VisualizationsD3/DonutD3/TryDonut.js
@@ -3,13 +3,13 @@ import { default as React, PropTypes } from 'react';
 export default class DonutD3 extends React.Component{
   render(){
     var dataset = [{age: 65, population: 65000}, {age: 35, population: 35000}]
-    var width = 960
+    var width = 1000
     var height = 500
     var radius = Math.min(width, height) / 2;
     var svg = d3.select("organicDonut")
     var arc = d3.svg.arc()
-      .outerRadius(radius - 10)
-      .innerRadius(radius - 70);
+      .outerRadius(radius - (height * .03))
+      .innerRadius(radius - (height * .17));
       var pie = d3.layout.pie()
         .sort(null)
         .value(function(d) {
@@ -23,14 +23,14 @@ export default class DonutD3 extends React.Component{
 
     return (
 
-      <div key={d.age + index}>
+      <div key={d.age + index} style={{textAlign: "center"}}>
+      <img src="../../icons/leaf-brown3x.png" style={{position: "absolute", left: "47%", top: "37%", zIndex:"2"}} width={width * .23} height={height * .44}>
+      </img>
         <svg class="organicDonut" width={width} height={height}>
             <g transform="translate(480,250)">
               <g key={d.age + index} className="arc">
                     <path key={d+index} d={arcCalc} style={pathStyle}>
                     </path>
-                    <image href="../../icons/crop-icons-yellow/hazelnut-yellow3x.png" width="220" height="220" x="-100" y="-110">
-                    </image>
               </g>
             </g>
         </svg>
